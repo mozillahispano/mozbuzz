dd76815cd3ecf4ee97c7e3b338f7af454f272098
YouWoTMA David Bengoa <david@bengoarocandio.com>
2011-11-11 12:31:55 +0100

    removed unecessary files, some css work (I still have to make a general css cleanuup)

--- media/js/search.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    $('.filter a.more').click(function(e) {
-        e.preventDefault();
-        $(this).closest('li').hide()
-        $('.extra', $(this).closest('ul')).show()
-    });
-
-});
