826ad7a57d6ae5628f76912b19b0b920a6d01886
YouWoTMA David Bengoa <david@bengoarocandio.com>
2012-01-29 13:55:01 +0100

    audience slider

--- buzz/search.py
+++ buzz/search.py
@@ -3,7 +3,16 @@ from mozbuzz.buzz.models import Mention
 VALID_PARMS = {
     "feedback_type":"set",
     "update_rate":"set",
-    "q":"str"
+    "q":"str",
+    "audience_gt":"int",
+    "audience_lt":"int",
+}
+
+DEFAULTS = {
+    "feedback_type": set(()),
+    "update_rate": set(()),
+    "audience_gt": 0,
+    "audience_lt": 7000000000,
 }
 
 def buzz_search(query):
@@ -13,13 +22,15 @@ def buzz_search(query):
     filters = {
         "feedback_type": lambda objs,val:objs.filter(feedback__in=val),
         "update_rate": lambda objs,val:objs.filter(update_rate__in=val),
-        "q":lambda objs,val:objs.filter(text__contains=word)
+        "q":lambda objs,val:objs.filter(text__contains=word),
+        "audience_gt": lambda objs, val: objs.filter(estimated_audience__gte=val),
+        "audience_lt": lambda objs, val: objs.filter(estimated_audience__lte=val),
     }
 
     for key,val in query.iteritems():
         type=VALID_PARMS[key]
 
-        if type=="set" and len(val):
+        if (type=="set" and len(val)) or type=="int":
             objs = filters[key](objs,val)
         elif type=="str":
             keywords = val.split()
@@ -37,8 +48,11 @@ def clean_query(query):
                 res[parm] = set(map(int,query[parm].split(",")))
             elif type=="str":
                 res[parm] = query[parm]
-        elif type=="set":
-            res[parm] = set(())
+            elif type=="int":
+                res[parm] = int(query[parm])
+
+        elif parm in DEFAULTS:
+            res[parm] = DEFAULTS[parm]
 
     return res
 
--- media/js/mozbuzz.js
+++ media/js/mozbuzz.js
@@ -22,4 +22,52 @@ $(document.body).delegate("a[data-althref]","click",function(e){
         .addClass("followups-show").removeClass("followups-hide").text(_("Show"))
         .parents(".mention").find(".followups").hide();
     return false;
-});;
+});
+
+
+
+
+
+(function(){
+
+    var slider_values = [0,25,50,100,250,500,1000,2500,5000,10000,25000,50000,100000,250000,500000,1000000,10000000,100000000,1000000000,7000000000];
+
+    function format_quantity(c){
+        c = Math.round(c) + "";
+        var n = "";
+        while(c.length >= 3){
+            n = "." + c.substr(-3,3) + n;
+            c = c.substr(0,c.length - 3);
+        }
+        if(c.length){
+            return c + n;
+        }else{
+            return n.substring(1);
+        }
+    }
+
+    function update_audience(s,e){
+        $("#audience-preview").text(_("Between %s and %s", format_quantity(s) , format_quantity(e)));
+    }
+
+    var start = parseInt($("#input_audience_audience_gt").val());
+    var end = parseInt($("#input_audience_audience_lt").val());
+
+    update_audience(start,end);
+
+    var audienceSlider = $("#slider-audience").slider({
+        range: true,
+        min: 0,
+        max: 19,
+        values: [slider_values.indexOf(start), slider_values.indexOf(end)],
+        slide: function( event, ui ) {
+            var s = slider_values[ui.values[0]], e = slider_values[ui.values[1]];
+            update_audience(s,e);
+            $("#input_audience_audience_gt").val(s);
+            $("#input_audience_audience_lt").val(e);
+            $("#audience-update").show();
+        }
+    });
+
+})();
+
