1447bcd90bfd55fae29ad2ce99722eed671d5ed7
YouWoTMA David Bengoa <david@bengoarocandio.com>
2012-01-29 11:50:00 +0100

    cambios followups

--- media/js/mozbuzz.js
+++ media/js/mozbuzz.js
@@ -1,8 +1,17 @@
 
+function _(x){
+    var args = arguments,i=1;
+    return x.replace(/%s/g,function(){
+        return args[i++];
+    });
+}
 
 $(document.body).delegate("a[data-althref]","click",function(e){
     if(e.shiftKey){
         location.href=this.getAttribute("data-althref");
         return false;
     }
+}).delegate("a.followups-show","click",function(){
+    $(this).parents(".mention").find(".followups").show();
+    return false;
 });
