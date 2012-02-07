8b6311871f6c4d17acab3d71d10d63be0cbedf34
YouWoTMA David Bengoa <david@bengoarocandio.com>
2012-02-07 22:15:09 +0100

    bugfixes

--- buzz/admin.py
+++ buzz/admin.py
@@ -24,7 +24,7 @@ class FileAdmin(BuzzAdmin):
 class MentionAdmin(BuzzAdmin):
     fields = ('type','origin', 'feedback', 'link', 'text', 'author_expertise',
               'previous_product_comments', 'estimated_audience', 'country',
-              'product', 'relevant_audience', 'update_rate', 'remarks')
+              'product', 'relevant_audience', 'update_rate', 'remarks', 'source_name')
 
 
 class FollowUpAdmin(BuzzAdmin):
