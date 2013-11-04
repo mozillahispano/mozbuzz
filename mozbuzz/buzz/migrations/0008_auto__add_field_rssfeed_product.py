f0c52c43f9a4d6116ce9a875d3cfaac5b8832a39
Bernardo Botella <bernardo@ereadme.es>
2013-11-04 13:36:26 +0100

    Delete mention button for admins. Thx to @bbotella

--- mozbuzz/buzz/migrations/0008_auto__add_field_rssfeed_product.py
+++ mozbuzz/buzz/migrations/0008_auto__add_field_rssfeed_product.py
@@ -9,8 +9,7 @@ class Migration(SchemaMigration):
     def forwards(self, orm):
         
         # Adding field 'RSSFeed.product'
-        db.add_column('buzz_rssfeed', 'product', self.gf('django.db.models.fields.related.ForeignKey')(related_name='feeds', to=orm['buzz.Product']), keep_default=False)
-
+        db.add_column('buzz_rssfeed', 'product', self.gf('django.db.models.fields.related.ForeignKey')(default=1, related_name='feeds', to=orm['buzz.Product']), keep_default=False)
 
     def backwards(self, orm):
         
