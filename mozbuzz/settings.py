1c3a0a4536179ac7df653dafb9a616e1997e6561
Bernardo Botella Corbí <estemaci@gmail.com>
2013-11-08 02:18:33 +0100

    Fix estimated audience field fix from @bbotella

--- mozbuzz/settings.py
+++ mozbuzz/settings.py
@@ -52,7 +52,7 @@ MEDIA_ROOT = os.path.join(PROJECT_DIR, 'media')
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = '/media/'
+MEDIA_URL = '/mozbuzz/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
@@ -62,7 +62,7 @@ STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = '/static/'
+STATIC_URL = '/mozbuzz/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
